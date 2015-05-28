da3cab255dfe150f9b16fa1aa407a0176a8cae16
Jeremy B. Merrill <jeremy.merrill@nytimes.com>
2015-05-28 17:33:30 -0400

    poke for rebuild

--- webapp/tabula_web.rb
+++ webapp/tabula_web.rb
@@ -10,6 +10,7 @@ require 'tempfile'
 require 'fileutils'
 require 'securerandom'
 
+
 require_relative '../lib/jars/tabula-extractor-0.7.4-SNAPSHOT-jar-with-dependencies.jar'
 
 require_relative '../lib/tabula_java_wrapper.rb'
